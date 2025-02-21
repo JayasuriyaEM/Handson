3925f64caf85419a7b4d9b3928cbe38081f5a803
jayasuriya <jayasuriya.c@eminds.ai>
2025-02-21 15:40:38 +0530

    initial commit

---
+++ src/main/java/linkedin/com/LinkedIn_Cron.java
@@ -0,0 +1,26 @@
+package linkedin.com;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LinkedIn_Cron {
+	public static void main(String args[]) throws InterruptedException {
+	
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+//		driver.get("https://www.linkedin.com/login");
+//		driver.findElement(By.id("username")).sendKeys("jayasuriyac2000@gmail.com");
+//		driver.findElement(By.id("password")).sendKeys("Jaya0411@ln");
+//		driver.findElement(By.xpath("//button[@type = 'submit']")).click();
+//		Thread.sleep(1000);
+		driver.get("https://www.amazon.com/");
+	}
+	
+}
